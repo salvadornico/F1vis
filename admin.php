c071d22b48a696f151f66881d750fb956679497c
Nico Salvador <salvador.nico@gmail.com>
2017-07-03 15:14:08 +0800

    Added favorite drivers summary to admin page

--- admin.php
+++ admin.php
@@ -32,24 +32,64 @@
 
 		<div class="section container">
 			
-			<h1>Admin Only</h1>
+			<h1 class="center-align">Admin Only</h1>
 
-			<h4 class="center-align">Registered User Count</h4>
-			<table class="centered bordered">
-				<tr>
-					<th class="center-align">Admin</th>
-					<th class="center-align">Regular</th>
-				</tr>
-				<tr>
-					<?php 
+			<div class="row">
 
-						echo "<td>$count_admin</td>";
-						echo "<td>$count_regular</td>";
+				<div class="col s12 m6">
+					
+					<h4 class="center-align">Most Favorited Drivers</h4>
+					<table class="centered bordered">
+						<tr>
+							<th class="center-align">Driver</th>
+							<th class="center-align">Users</th>
+						</tr>
 
-					?>
-				</tr>
-			</table>
-			
+						<?php 
+
+							// Get number of regular users
+							$regular_result = querySQL("SELECT CONCAT(drivers.forename, ' ', drivers.surname) AS 'driverName',
+							 COUNT(favoritedrivers.userId) as 'userCount' 
+							 FROM drivers JOIN favoritedrivers ON favoritedrivers.driverId = drivers.driverId 
+							 GROUP BY driverName ORDER BY userCount DESC, driverName");
+
+							if (mysqli_num_rows($regular_result) > 0) {
+								while ($row = mysqli_fetch_assoc($regular_result)) {
+									extract($row);
+									$driverName = utf8_encode($driverName);
+
+									echo "<tr><td>$driverName</td><td>$userCount</td></tr>";
+								}
+							}
+
+						?>
+
+					</table>
+
+				</div> <!-- /favorites div -->			
+
+				<div class="col s12 m6">
+					
+					<h4 class="center-align">Registered User Count</h4>
+					<table class="centered bordered">
+						<tr>
+							<th class="center-align">Admin</th>
+							<th class="center-align">Regular</th>
+						</tr>
+						<tr>
+							<?php 
+
+								echo "<td>$count_admin</td>";
+								echo "<td>$count_regular</td>";
+
+							?>
+						</tr>
+					</table>
+
+				</div> <!-- /users div -->
+				
+			</div>
+
 		</div> <!-- /container -->
 		
 	</main>
